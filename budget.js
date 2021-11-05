be7b404a93a9097e58a5cfc152c280b29ad7c2cc
JabenJu <89491072+JabenJu@users.noreply.github.com>
2021-11-05 11:28:42 -0400

    Help me

--- budget.js
+++ budget.js
@@ -1,17 +1,30 @@
 let budget = 0;
 let expensesTotal = 0;
 
-function budgetSetter(){
-    budget = document.getElementById("loginPassword").value
-}
-
 function expenseAdd(){
     let expenseNames = document.getElementById("expenseNames")
     let expenseValues = document.getElementById("expenseValues")
-    let nameHtml = expenseNames.innerHTML
-    let valueHtml = expenseValues.innerHTML
-    nameHtml += "\n"+(document.getElementById("expenseNameUi").val())
-    valueHtml += "\n"+(document.getElementById("expenseAmountUi").val())
-    expensesTotal += document.getElementById("expenseAmountUi").val()
-    updateBalance()
+    
+    let pNode1 = document.createElement("p")
+    let pNode2 = document.createElement("p")
+
+    expenseNames.append(("\n"+document.getElementById("expenseNameUi").value),pNode1)
+    expenseValues.append(("\n"+document.getElementById("expenseAmountUi").value),pNode2)
+
+    expensesTotal += Number(document.getElementById("expenseAmountUi").value)
+    $("#expenseJS").html("$" + expensesTotal)
+    balanceUpdate()
+}
+
+function budgetUpdate()
+{
+    budget = Number($("#budgetUi").val())
+    $("#budgetJS").html("$" + budget)
+    balanceUpdate()
+}
+
+function balanceUpdate()
+{
+    let balance = budget - expensesTotal
+    $("#balanceJS").html("$" + balance)
 }
