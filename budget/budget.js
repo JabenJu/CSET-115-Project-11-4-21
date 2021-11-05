a6cc4571120c3f360983fba9dfb04365e872922c
YolKay <alfonsecak@yahoo.com>
2021-11-05 10:16:18 -0400

    Moved items into a folder

---
+++ budget/budget.js
@@ -0,0 +1,18 @@
+let budget = document.getElementById("budgetJS");
+let expensesTotal = document.getElementById("expenseJS");
+let balance = document.getElementById("balanceJS");
+
+
+function budgetSetter(){
+    budget = document.getElementById("").value
+    
+}
+
+function expenseAdd(){
+    expName = document.getElementById("expenseNameUi").value
+    expAmount = document.getElementById("expenseAmountUi").value
+    expAmount = Number(expAmount)
+    
+}
+
+function
