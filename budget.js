bd6db2f2ada59513a395c8e216ec528d832672a2
JabenJu <89491072+JabenJu@users.noreply.github.com>
2021-11-05 10:33:53 -0400

    Expenses functions js

--- budget.js
+++ budget.js
@@ -1,14 +1,17 @@
 let budget = 0;
 let expensesTotal = 0;
-let balance = budget - expensesTotal;
-
 
 function budgetSetter(){
     budget = document.getElementById("loginPassword").value
 }
 
 function expenseAdd(){
-    expName = document.getElementById("expenseNameUi").value
-    expAmount = document.getElementById("expenseAmountUi").value
-
+    let expenseNames = document.getElementById("expenseNames")
+    let expenseValues = document.getElementById("expenseValues")
+    let nameHtml = expenseNames.innerHTML
+    let valueHtml = expenseValues.innerHTML
+    nameHtml += "\n"+(document.getElementById("expenseNameUi").val())
+    valueHtml += "\n"+(document.getElementById("expenseAmountUi").val())
+    expensesTotal += document.getElementById("expenseAmountUi").val()
+    updateBalance()
 }
